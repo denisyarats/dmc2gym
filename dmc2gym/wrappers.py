cad63a4f2d92a61baee11aa7101c057e53ff8bd0
Denis Yarats <denisyarats@gmail.com>
2019-10-09 14:13:17 -0700

    save internal state

--- dmc2gym/wrappers.py
+++ dmc2gym/wrappers.py
@@ -52,23 +52,30 @@ class DMCWrapper(core.Env):
         self._width = width
         self._camera_id = camera_id
         self._frame_skip = frame_skip
+        
+        # create task
         self._env = suite.load(
             domain_name=domain_name,
             task_name=task_name,
             task_kwargs=task_kwargs,
             visualize_reward=visualize_reward,
             environment_kwargs=environment_kwargs)
+        
+        # true and normalized action spaces
         self._true_action_space = _spec_to_box([self._env.action_spec()])
         self._norm_action_space = spaces.Box(
-            -1.0 * np.ones(self._true_action_space.shape),
-            +1.0 * np.ones(self._true_action_space.shape),
-            dtype=np.float32)
+            low=-1.0, high=1.0, shape=self._true_action_space.shape, dtype=np.float32) 
+        
+        # create observation space
         if from_pixels:
             self._observation_space = spaces.Box(
                 low=0, high=255, shape=[3, height, width], dtype=np.uint8)
         else:
             self._observation_space = _spec_to_box(
                 self._env.observation_spec().values())
+            
+        self._internal_state_space = spaces.Box(
+            low=-np.inf, high=np.inf, shape=self._env.physics.get_state().shape, dtype=np.float32)
 
     def __getattr__(self, name):
         return getattr(self._env, name)
@@ -98,6 +105,10 @@ class DMCWrapper(core.Env):
         return self._observation_space
 
     @property
+    def internal_state_space(self):
+        return self._internal_state_space
+
+    @property
     def action_space(self):
         return self._norm_action_space
 
@@ -111,6 +122,10 @@ class DMCWrapper(core.Env):
         action = self._convert_action(action)
         assert self._true_action_space.contains(action)
         reward = 0
+        extra = {
+            'internal_state': self._env.physics.get_state().copy()
+        }
+        
         for _ in range(self._frame_skip):
             time_step = self._env.step(action)
             reward += time_step.reward or 0
@@ -118,8 +133,8 @@ class DMCWrapper(core.Env):
             if done:
                 break
         obs = self._get_obs(time_step)
-        info = {'discount': time_step.discount}
-        return obs, reward, done, info
+        extra['discount'] = time_step.discount
+        return obs, reward, done, extra
 
     def reset(self):
         time_step = self._env.reset()
@@ -130,5 +145,6 @@ class DMCWrapper(core.Env):
         assert mode == 'rgb_array', 'only support rgb_array mode, given %s' % mode
         height = height or self._height
         width = width or self._width
+        camera_id = camera_id or self._camera_id
         return self._env.physics.render(
             height=height, width=width, camera_id=camera_id)
