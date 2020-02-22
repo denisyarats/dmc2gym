b3d13d3ac9d262b3944b5808f3425c1d01e2d4ff
Ge Yang <yangge1987@gmail.com>
2020-02-21 18:09:49 -0800

    handle None value, do not propagate to gym environment.

--- dmc2gym/__init__.py
+++ dmc2gym/__init__.py
@@ -3,17 +3,18 @@ from gym.envs.registration import register
 
 
 def make(
-    domain_name,
-    task_name,
-    seed=1,
-    visualize_reward=True,
-    from_pixels=False,
-    height=84,
-    width=84,
-    camera_id=0,
-    frame_skip=1,
-    episode_length=1000,
-    environment_kwargs=None
+        domain_name,
+        task_name,
+        seed=1,
+        visualize_reward=True,
+        from_pixels=False,
+        height=84,
+        width=84,
+        camera_id=0,
+        frame_skip=1,
+        episode_length=1000,
+        environment_kwargs=None,
+        time_limit=None
 ):
     env_id = 'dmc_%s_%s-v1' % (domain_name, task_name)
 
@@ -24,23 +25,26 @@ def make(
     max_episode_steps = (episode_length + frame_skip - 1) // frame_skip
 
     if not env_id in gym.envs.registry.env_specs:
+        task_kwargs = {}
+        if seed is not None:
+            task_kwargs['seed'] = seed
+        if time_limit is not None:
+            task_kwargs['time_limit'] = time_limit
         register(
             id=env_id,
             entry_point='dmc2gym.wrappers:DMCWrapper',
-            kwargs={
-                'domain_name': domain_name,
-                'task_name': task_name,
-                'task_kwargs': {
-                    'random': seed
-                },
-                'environment_kwargs': environment_kwargs,
-                'visualize_reward': visualize_reward,
-                'from_pixels': from_pixels,
-                'height': height,
-                'width': width,
-                'camera_id': camera_id,
-                'frame_skip': frame_skip,
-            },
-            max_episode_steps=max_episode_steps
+            kwargs=dict(
+                domain_name=domain_name,
+                task_name=task_name,
+                task_kwargs=task_kwargs,
+                environment_kwargs=environment_kwargs,
+                visualize_reward=visualize_reward,
+                from_pixels=from_pixels,
+                height=height,
+                width=width,
+                camera_id=camera_id,
+                frame_skip=frame_skip,
+            ),
+            max_episode_steps=max_episode_steps,
         )
     return gym.make(env_id)
