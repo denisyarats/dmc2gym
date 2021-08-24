16e3df7b2aa0731d8693ba198dc8c44653e9b431
YawenDuan <kmdanielduan@gmail.com>
2021-08-24 07:16:22 -0700

    reward should be float

--- dmc2gym/wrappers.py
+++ dmc2gym/wrappers.py
@@ -152,12 +152,12 @@ class DMCWrapper(core.Env):
         assert self._norm_action_space.contains(action)
         action = self._convert_action(action)
         assert self._true_action_space.contains(action)
-        reward = 0
+        reward = float(0)
         extra = {'internal_state': self._env.physics.get_state().copy()}
 
         for _ in range(self._frame_skip):
             time_step = self._env.step(action)
-            reward += time_step.reward or 0
+            reward += time_step.reward or float(0)
             done = time_step.last()
             if done:
                 break
