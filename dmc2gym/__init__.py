9e28e05c0547fcbd2c9f62f099ecd95d068221f6
YawenDuan <kmdanielduan@gmail.com>
2021-08-24 22:25:39 -0700

    add direct access of some key class and function

--- dmc2gym/__init__.py
+++ dmc2gym/__init__.py
@@ -1,2 +1,7 @@
 # MODIFIED BY Yawen Duan (https://github.com/kmdanielduan)
 # moving the whole register process to registration.py
+
+from dmc2gym.wrappers import DMCWrapper
+from dmc2gym.registration import register_suite, register_env
+
+__all__ = ["DMCWrapper", "register_suite", "register_env"]
